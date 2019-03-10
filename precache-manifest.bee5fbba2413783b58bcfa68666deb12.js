907a2f2cec4099a490d570d1103a346139b9fbd9
Harscoët <harscoet_florian@hotmail.fr>
2019-03-10 16:01:53 +0100

    Updates

---
+++ precache-manifest.bee5fbba2413783b58bcfa68666deb12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7eb980d549d66798bfde",
+    "url": "/Tinder-philo/static/css/main.f887536e.chunk.css"
+  },
+  {
+    "revision": "7eb980d549d66798bfde",
+    "url": "/Tinder-philo/static/js/main.b491ee39.chunk.js"
+  },
+  {
+    "revision": "adb0b7cebefb082e4ece",
+    "url": "/Tinder-philo/static/js/runtime~main.a065d68e.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/Tinder-philo/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tinder-philo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0212d47dce3e2a680064ac7f6259b6be",
+    "url": "/Tinder-philo/index.html"
+  }
+];
